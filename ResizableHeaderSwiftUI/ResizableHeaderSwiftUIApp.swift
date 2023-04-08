7122e87412073a4940829a0db5e8cce47817f634
Max Valek <valek.maxwell@gmail.com>
2023-04-08 13:06:13 -0500

    Initial Commit

---
+++ ResizableHeaderSwiftUI/ResizableHeaderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResizableHeaderSwiftUIApp.swift
+//  ResizableHeaderSwiftUI
+//
+//  Created by Max Valek on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ResizableHeaderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
